f8844155a83fcece84a8cbfcf04dd205b6deea10
Guilherme Aurélio <guilhermeaurelio65@gmail.com>
2023-04-11 11:23:46 -0300

    Primeiro commit com as classes Pessoa e Animal

---
+++ animal.py
@@ -0,0 +1,29 @@
+from sys import setter
+class Animal:
+    def __init__(self, nome, peso, posicao):
+        self.__nome = nome
+        self.__peso = peso
+        self.__posicao = posicao
+    
+    @property
+    def nome (self):
+        return self.__nome
+    
+    @property
+    def peso (self):
+        return self.__peso
+    
+    @setter.peso()
+    def peso (self, peso):
+        self.__peso = peso
+    
+    @property
+    def posicao (self):
+        return self.__posicao
+    
+    @setter.posicao()
+    def posicao(self, pos):
+        self.__posicao = pos
+    
+    def moverse(self):
+        self.__posicao += 1
---
+++ pessoa.py
@@ -0,0 +1,7 @@
+class Pessoa:
+    """ A classe Pessoa representa as caracteristicas de uma pessoa
+        no contexto do ambiente acadêmico
+    """
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula
