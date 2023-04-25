a40d45ca9d3976cbde0939ee34c9d02c2b541a9a
Guilherme Aurélio <guilhermeaurelio65@gmail.com>
2023-04-25 11:33:24 -0300

    Criando estrutura de dados pilha.py

---
+++ aula08-pilha/pilha.py
@@ -0,0 +1,23 @@
+# -*- coding:UTF-8 -*-
+
+class Pilha:
+    def __init__(self) -> None:
+        self.__pilha = [] # list do python
+
+    def is_empty(self):
+        return True if len(self.__pilha) == 0 else False
+
+    def push(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def list_items(self):
+        pass
+
+    def get_size(self):
+        pass
